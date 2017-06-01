688de7c4ff0e95f3f6537e5fa59e6d055cab2dc0
Vincenzo Palazzo <vpalazzo@gmail.com>
2017-06-01 17:37:13 +0200

    - Aggiornato a 2017.05.01
    - Preinstallo sed e gzip che occorrono allo script di aggiornamento di
      glibc durante la generazione dei locales.
    
    - Adesso uso alpine:3.6

--- docker-archlinux.sh
+++ docker-archlinux.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-set -ue
+set -uex
 
 # MIRROR='https://mirrors.kernel.org/archlinux'
 MIRROR='https://mirror.f4st.host/archlinux'
-ISO_VER='2017.03.01'
+ISO_VER='2017.05.01'
 NAME="vpalazzo/archlinux"
 
 ENTRYPOINT='#!/bin/sh
@@ -19,6 +19,7 @@ FROM scratch
 ADD root.x86_64 /
 
 RUN \
+    set -x &&\
     ln -nfs /usr/share/zoneinfo/Europe/Rome /etc/localtime &&\
     echo "en_US.UTF-8 UTF-8" >/etc/locale.gen &&\
     echo "LANG=en_US.UTF-8" >/etc/locale.conf &&\
@@ -27,7 +28,8 @@ RUN \
     pacman-key --populate archlinux &&\
     echo "Server = http://mirror.f4st.host/archlinux/\$repo/os/\$arch" \
          >/etc/pacman.d/mirrorlist &&\
-    pacman --noconfirm -Syu sed &&\
+    pacman --noconfirm -Sy sed gzip &&\
+    pacman --noconfirm -Su &&\
     sed -i~ '"'"'/^#\[multilib\]$/{s/^#//;n;s/^#//}'"'"' /etc/pacman.conf &&\
     pacman --noconfirm -Sy
 
@@ -42,7 +44,7 @@ CMD [ "/usr/bin/bash", "-l" ]
 '
 
 DOCKERFILE_BUILDER='
-FROM alpine:3.5
+FROM alpine:3.6
 
 RUN apk --update add curl
 
