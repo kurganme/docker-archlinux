782ec3618b095460faa2ef824540acee6e54d0d5
Vincenzo Palazzo <vpalazzo@gmail.com>
2018-01-11 14:44:11 +0100

    Adesso utilizzo il multistage di docker :-)

--- docker-archlinux.sh
+++ docker-archlinux.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -uex
+set -ue
 
 # MIRROR='https://mirrors.kernel.org/archlinux'
 MIRROR='https://mirror.f4st.host/archlinux'
@@ -14,12 +14,22 @@ exec "$@"
 '
 
 DOCKERFILE='
-FROM scratch
+FROM alpine:3.7 as builder
 
-ADD root.x86_64 /
+RUN apk --no-cache add curl
 
 RUN \
-    set -x &&\
+    cd &&\
+    curl -L '"'${MIRROR}/iso/${ISO_VER}/`
+           `archlinux-bootstrap-${ISO_VER}-x86_64.tar.gz'"' \
+        | tar xz
+
+FROM scratch
+
+COPY --from=builder /root/root.x86_64/ /
+
+RUN \
+    set -- &&\
     ln -nfs /usr/share/zoneinfo/Europe/Rome /etc/localtime &&\
     echo "en_US.UTF-8 UTF-8" >/etc/locale.gen &&\
     echo "LANG=en_US.UTF-8" >/etc/locale.conf &&\
@@ -43,26 +53,8 @@ ENTRYPOINT [ "/entrypoint.sh" ]
 CMD [ "/usr/bin/bash", "-l" ]
 '
 
-DOCKERFILE_BUILDER='
-FROM alpine:3.6
-
-RUN apk --update add curl
-
-RUN \
-    mkdir /root/src/ &&\
-    curl -L '"'${MIRROR}/iso/${ISO_VER}/`
-           `archlinux-bootstrap-${ISO_VER}-x86_64.tar.gz'"' \
-        | tar xzC /root/src/
-
-RUN \
-    echo '"'$(echo "$DOCKERFILE" | base64 --wrap=0)'"' |\
-        base64 -d >/root/src/Dockerfile
-'
-
-echo "$DOCKERFILE_BUILDER" \
-    | docker build --force-rm --pull --tag "$NAME"-builder:"$ISO_VER" -
-
-docker run --rm "$NAME"-builder:"$ISO_VER" tar cC /root/src . \
-    | docker build --force-rm --tag "$NAME":"$ISO_VER" -
+echo "$DOCKERFILE" \
+    | docker build --force-rm --tag "$NAME"-builder:"$ISO_VER" - \
+             > /tmp/"$(basename "$0")".log
 
 exec docker run -ti --rm "$NAME":"$ISO_VER" "$@"
