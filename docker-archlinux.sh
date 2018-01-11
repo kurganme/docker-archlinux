e161b2e9c38a8bdd38d1b6c2eb8faba388e67b1c
Vincenzo Palazzo <vpalazzo@gmail.com>
2018-01-11 14:52:50 +0100

    Portato alla versione 2018.01.01

--- docker-archlinux.sh
+++ docker-archlinux.sh
@@ -3,7 +3,7 @@ set -ue
 
 # MIRROR='https://mirrors.kernel.org/archlinux'
 MIRROR='https://mirror.f4st.host/archlinux'
-ISO_VER='2017.11.01'
+ISO_VER='2018.01.01'
 NAME="vpalazzo/archlinux"
 
 ENTRYPOINT='#!/bin/sh
@@ -19,28 +19,24 @@ FROM alpine:3.7 as builder
 RUN apk --no-cache add curl
 
 RUN \
-    cd &&\
-    curl -L '"'${MIRROR}/iso/${ISO_VER}/`
-           `archlinux-bootstrap-${ISO_VER}-x86_64.tar.gz'"' \
-        | tar xz
+  cd &&\
+  curl -L '"'${MIRROR}/iso/${ISO_VER}/`
+          `archlinux-bootstrap-${ISO_VER}-x86_64.tar.gz'"' \
+  | tar xz
 
 FROM scratch
 
 COPY --from=builder /root/root.x86_64/ /
 
 RUN \
-    set -- &&\
+    set -x -- &&\
     ln -nfs /usr/share/zoneinfo/Europe/Rome /etc/localtime &&\
     echo "en_US.UTF-8 UTF-8" >/etc/locale.gen &&\
     echo "LANG=en_US.UTF-8" >/etc/locale.conf &&\
-    rm /etc/ssl/certs/ca-certificates.crt &&\
     pacman-key --init &&\
     pacman-key --populate archlinux &&\
-    echo "Server = http://mirror.f4st.host/archlinux/\$repo/os/\$arch" \
+    echo "Server = '"${MIRROR}"'/\$repo/os/\$arch" \
          >/etc/pacman.d/mirrorlist &&\
-    pacman --noconfirm -Sy sed gzip &&\
-    pacman --noconfirm -Su ca-certificates-utils &&\
-    sed -i~ '"'"'/^#\[multilib\]$/{s/^#//;n;s/^#//}'"'"' /etc/pacman.conf &&\
     pacman --noconfirm -Sy
 
 RUN \
@@ -54,7 +50,7 @@ CMD [ "/usr/bin/bash", "-l" ]
 '
 
 echo "$DOCKERFILE" \
-    | docker build --force-rm --tag "$NAME"-builder:"$ISO_VER" - \
+    | docker build --force-rm --tag "$NAME":"$ISO_VER" - \
              > /tmp/"$(basename "$0")".log
 
 exec docker run -ti --rm "$NAME":"$ISO_VER" "$@"
